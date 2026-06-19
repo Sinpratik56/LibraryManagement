3832e42721e9dba5059111de2d2c9cd5ad87d758
Pratik Singh <pratik.18027@gmail.com>
2026-06-19 17:57:30 +0530

    Add password change feature

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -194,6 +194,64 @@ const deleteUser = async (
     }
 };
 
+const changePassword = async (req, res) => {
+
+    try {
+
+        const {
+            currentPassword,
+            newPassword
+        } = req.body;
+
+        const user = await User.findById(
+            req.user.id
+        );
+
+        if (!user) {
+
+            return res.status(404).json({
+                message: "User Not Found"
+            });
+        }
+
+        const isMatch =
+            await bcrypt.compare(
+                currentPassword,
+                user.password
+            );
+
+        if (!isMatch) {
+
+            return res.status(400).json({
+                message:
+                    "Current Password Incorrect"
+            });
+        }
+
+        const salt =
+            await bcrypt.genSalt(10);
+
+        user.password =
+            await bcrypt.hash(
+                newPassword,
+                salt
+            );
+
+        await user.save();
+
+        res.status(200).json({
+            message:
+                "Password Changed Successfully"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
 
     registerUser,
