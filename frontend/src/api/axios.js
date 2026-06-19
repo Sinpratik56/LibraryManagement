757f8a855a2966b809a3325a9b16143a2d64e758
Pratik Singh <pratik.18027@gmail.com>
2026-06-19 16:18:21 +0530

    Connected frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://librarymanagement-fy8k.onrender.com/api"
 });
 
 API.interceptors.request.use(
